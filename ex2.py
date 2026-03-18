a562475c02754084e297ceb641463185f2b79706
malulattaa <maria.latta2@estudante.ifms.edu.br>
2026-03-17 22:52:37 -0400

    final

--- ex2.py
+++ ex2.py
@@ -1,28 +1,13 @@
-"""Implemente um verificador de parênteses confirmando se os mesmo estão balanceados.
-
-Exemplo:
-
-Válido:
-((A+B) * C)
-Inválido:
-(A+B))
-
-Teste:
-
-((A+B) * C)
-(A+B))
-((A+B)"""
-
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
 class Pilha:
     def __init__(self):
         self.topo = None
         self._size = 0
-        
+    
     def push(self, value):
         node = Node(value)
         node.next = self.topo
@@ -33,37 +18,31 @@ class Pilha:
         if self.topo is None:
             return None
         aux = self.topo
-        self.topo = self.topo.next 
+        self.topo = self.topo.next
         self._size -= 1
         return aux.value
-    
-    
-emp = Pilha()
 
-abrirParenteses = '('
-fecharParenteses = ')'
-contAbrir = 0
-contFechar = 0
+def verificar_parenteses(value):
+    p = Pilha()
+    valido = True
 
+    for elem in value:
+        if elem == '(':
+            p.push(elem)
+        elif elem == ')':
+            if p.topo is None:
+                valido = False
+            p.pop()
+
+    if p.topo is not None:
+        valido = False
+    
+    return valido
+        
 entrada = input("Digite a entrada: ")
 
-for p in entrada:
-    emp.push(p)
-
-while emp.topo is not None:
-    elemento = emp.pop()
-    print(elemento)
-    
-    for desemp in elemento:
-        if desemp == fecharParenteses:
-            contFechar += 1 
-        if desemp == abrirParenteses:
-            contAbrir += 1 
-
-print(contAbrir)
-print(contFechar)
-if contAbrir == contFechar:
-    print("Válido")
-else:
-    print("Inválido")
+if verificar_parenteses(entrada):
+    print('Válido')
+else: 
+    print('Inválido')
         
